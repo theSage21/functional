ebebbb344a1ffff6f2a299e4e46365c2c26a3cad
Arjoonn Sharma <arjoonn.94@gmail.com>
2019-05-04 09:16:06 +0530

    power

--- src/numbers.py
+++ src/numbers.py
@@ -1,62 +1,50 @@
-def zero(x):
+# How to have the 'concept' of numbers?
+
+# Let counting happen by how many times you apply a function to an argument
+def zero(x):  # don't call the function x
     def fn(y):
-        return y
+        return y  # apply zero times
     return fn
 
 def one(x):
     def fn(y):
-        return x(y)
+        return x(y)  # apply once
     return fn
 
 def two(x):
     def fn(y):
-        return x(x(y))
+        return x(x(y))  # apply twice
     return fn
 
-def show(x):
-    print(x)
-    return x
+def incr(x):  # cheating. Just so that we can verify that numbers are actually working
+    return x+1
 
 
-zero(show)(1)  # nothing
-print('-'*10)
-one(show)(1)  # 1
-print('-'*10)
-two(show)(1)  # 1 1
-print('-'*10)
+print(zero(incr)(0) == 0)
+print(one(incr)(0) == 1)
+print(two(incr)(0) == 2)
+
+
+# It's tiring to write all the definitions. I need to GENERATE the next number
 
 
 def next_number(num):
     def n_num(f):
         def fn(x):
-            r = num(f)(x)
-            return f(r)
+            r = num(f)(x)  # apply as many times as given num
+            return f(r)  # apply once more
         return fn
     return n_num
-
-def incr(x):
-    return x + 1
 
 three = next_number(two)
 four = next_number(three)
 five = next_number(four)
 
-print(three(incr)(0))
-print('-'*10)
-print(four(incr)(0))
+print(three(incr)(0) == 3)
+print(four(incr)(0) == 4)
 
-
-def mult(a):
-    def fn(b):
-        def result(x):
-            return a(b(x))
-        return result
-    return fn
-print('--- mult')
-print(mult(three)(four)(incr)(0))
-print(mult(three)(three)(incr)(0))
-print(mult(two)(five)(incr)(0))
-
+# How to add???
+# apply x times then apply y times.
 
 def add(a):
     def fn(b):
@@ -65,8 +53,61 @@ def add(a):
         return result
     return fn
 
-print('--- add')
-print(add(three)(four)(incr)(0))
-print(add(three)(three)(incr)(0))
-print(add(one)(two)(incr)(0))
-print(add(zero)(two)(incr)(0))
+
+# all this def+return is too much typing
+def add(a):
+    def fn(b):
+        return lambda x: a(next_number)(b)(x)
+    return fn
+
+def add(a):
+    return lambda b: lambda x: a(next_number)(b)(x)
+
+add = lambda a: lambda b: lambda x: a(next_number)(b)(x)
+
+print(add(three)(four)(incr)(0) == 7)
+print(add(three)(three)(incr)(0) == 6)
+print(add(one)(two)(incr)(0) == 3)
+print(add(zero)(two)(incr)(0) == 2)
+
+def mult(a):
+    def fn(b):
+        def result(x):
+            return a(b(x))
+        return result
+    return fn
+
+def mult(a):
+    def fn(b):
+        return lambda x: a(b(x))
+    return fn
+
+def mult(a):
+    return lambda b: lambda x: a(b(x))
+
+mult = lambda a: lambda b: lambda x: a(b(x))
+# mult = λa:λb:λx:a(b(x))
+# mult = λabx:a(b(x))
+# mult = λabx:abx
+# mult = λabx.abx
+
+# This is the notation that appears in most papers
+
+
+print(mult(two)(three)(incr)(0) == 6)
+print(mult(two)(two)(incr)(0) == 4)
+print(mult(five)(three)(incr)(0) == 15)
+
+def power(a):
+    def fn(b):
+        def result(x):
+            return a(b)(x)
+        return result
+    return fn
+
+power = lambda a: lambda b: lambda x: b(a)(x)
+
+print(power(two)(three)(incr)(0) == 8)
+print(power(five)(two)(incr)(0) == 25)
+print(power(five)(zero)(incr)(0) == 1)
+print(power(five)(one)(incr)(0) == 5)

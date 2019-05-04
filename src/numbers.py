cb864a9ff7124e4a8c150574098dc88152160070
Arjoonn Sharma <arjoonn.94@gmail.com>
2019-05-04 06:58:35 +0530

    add + multiply

--- src/numbers.py
+++ src/numbers.py
@@ -28,15 +28,45 @@ print('-'*10)
 
 def next_number(num):
     def n_num(f):
-        def fn(y):
-            r = num(f)(y)
+        def fn(x):
+            r = num(f)(x)
             return f(r)
         return fn
     return n_num
 
+def incr(x):
+    return x + 1
+
 three = next_number(two)
 four = next_number(three)
+five = next_number(four)
 
-three(show)(1)
+print(three(incr)(0))
 print('-'*10)
-four(show)(1)
+print(four(incr)(0))
+
+
+def mult(a):
+    def fn(b):
+        def result(x):
+            return a(b(x))
+        return result
+    return fn
+print('--- mult')
+print(mult(three)(four)(incr)(0))
+print(mult(three)(three)(incr)(0))
+print(mult(two)(five)(incr)(0))
+
+
+def add(a):
+    def fn(b):
+        def result(x):
+            return a(next_number)(b)(x)
+        return result
+    return fn
+
+print('--- add')
+print(add(three)(four)(incr)(0))
+print(add(three)(three)(incr)(0))
+print(add(one)(two)(incr)(0))
+print(add(zero)(two)(incr)(0))
